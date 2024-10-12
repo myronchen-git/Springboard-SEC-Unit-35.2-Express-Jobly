bf067f56c655753db43c6355573da5fc6dc241b8
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-10-11 18:51:56 -0700

    fix: drop databases if exist
    The "DROP DATABASE" SQL statement was missing the "IF EXISTS"
    condition.  This caused an error when running the jobly.sql file.
    There is currently no negative impact of having the error, but to
    reduce possible issues in the future, the condition is now added.

--- jobly.sql
+++ jobly.sql
@@ -1,7 +1,7 @@
 \echo 'Delete and recreate jobly db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
 
-DROP DATABASE jobly;
+DROP DATABASE IF EXISTS jobly;
 CREATE DATABASE jobly;
 \connect jobly
 
@@ -11,7 +11,7 @@ CREATE DATABASE jobly;
 \echo 'Delete and recreate jobly_test db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
 
-DROP DATABASE jobly_test;
+DROP DATABASE IF EXISTS jobly_test;
 CREATE DATABASE jobly_test;
 \connect jobly_test
 
